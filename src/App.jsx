c3e7fab7fc135f427af252a2964451330f1b7aad
cylee <cylee@merimen.com>
2024-04-05 15:26:47 +0800

    inital-commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
